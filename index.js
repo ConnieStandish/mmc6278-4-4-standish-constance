a541fc09fbff1e8daf03d2abc8b0f7b7d1ef71c8
Connie Standish <conniestand@gmail.com>
2022-06-02 16:13:15 -0400

    displayed variables

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//display variables
+
+var wordToGuess = document.getElementById('word-to-guess')
+var previousWord = document.getElementById('previous-word')
+var incorrectLetters = document.getElementById('incorrect-letters')
+var remainingGuesses = document.getElementById('remaining-guesses')
+
+var remainingGuesses = 10
+var wins = 0
+var losses = 0
+
 var words = [
   'bananas',
   'grapes',
