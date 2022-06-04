4e99ac71072f31bd73ba25db12d3a0583434957e
Connie Standish <conniestand@gmail.com>
2022-06-04 09:03:29 -0400

    added comment section, still troubleshooting.

--- index.js
+++ index.js
@@ -24,13 +24,15 @@ var remainingGuesses = 10
 var wins = 0
 var losses = 0
 
+//Generate random word
 var game = words[Math.floor(Math.random() * words.length)]
 console.log(game)
 wordToGuess.textContent = game
+game.replace([a-z]/gi, '_')
 
-var displayHiddenWord = function() {
-  game.replace([a-z]/gi, '_')
-}
+// var displayHiddenWord = function() {
+//   game.replace([a-z]/gi, '_')
+// }
 
 document.onkeyup = function(event) {
   
