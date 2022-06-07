3d826f239cb638f8f32d396f32604b4df57614e2
Connie Standish <conniestand@gmail.com>
2022-06-07 10:17:50 -0400

    made improvements to program by declaring additional global variables and creating a reset function

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ var wordToGuess = document.querySelector('#word-to-guess')
 var previousWord = document.getElementById('previous-word')
 var incorrectLetters = document.getElementById('incorrect-letters')
 var remainingGuesses = document.getElementById('remaining-guesses')
+var gameWin = document.getElementById('wins')
+var gameLoss = document.getElementById('losses')
 
-// var remainingGuesses = 10
+var wordsLeft = 10
 var wins = 0
 var losses = 0
 
@@ -31,21 +33,25 @@ var str = words[Math.floor(Math.random() * words.length)]
 wordToGuess.textContent = str.replace(/[abcdefghijklmnopqrstuvwxyz]/g, '_')
 
 //Display 10 guesses
-remainingGuesses.textContent = 10
+remainingGuesses.textContent = wordsLeft
 
 
 //Set keyboard event
 var correctGuess = []
 var wrongGuess = []
 
-document.onkeyup = function(e) {
+document.onkeyup = function (e) {
   // e.preventDefault()
   var letters = 'abcdefghijklmnopqrstuvwxyzd'
   if (!letters.includes(e.key)) return
 
-  if (str.includes(e.key)) {
+  if (str.includes(e.key) && !correctGuess.includes(e.key)) {
+
     correctGuess.push(e.key)
-  } else {
+
+  } else if (!wrongGuess.includes(e.key)) {
+    wordsLeft--
+    remainingGuesses.textContent = wordsLeft
     wrongGuess.push(e.key)
   }
 
@@ -56,26 +62,56 @@ document.onkeyup = function(e) {
     } else {
       wordToGuess.textContent += '_'
       incorrectLetters.textContent = wrongGuess
-      
+
     }
-  
   }
+  //handle win or loss
+  //win is no more underscores and more than 0 remaining guesses
+  //loss would be reach 0 and answer incomplete
+
+  if (wordToGuess.textContent === str && wordsLeft > 0) {
+    wins++
+    gameWin.textContent = wins
+    // previousWord.textContent = str
+    // str = words[Math.floor(Math.random() * words.length)]
+    // wordToGuess.textContent = str.replace(/[abcdefghijklmnopqrstuvwxyz]/g, '_')
+    // wordsLeft = 10
+    // remainingGuesses.textContent = wordsLeft
+    // incorrectLetters.textContent = ""
+    // correctGuess = []
+    // wrongGuess = []
+    reset()
+  } else if (wordToGuess.textContent !== str && wordsLeft <= 0) {
+    losses++
+    gameLoss.textContent = losses
+    // previousWord.textContent = str
+    // str = words[Math.floor(Math.random() * words.length)]
+    // wordToGuess.textContent = str.replace(/[abcdefghijklmnopqrstuvwxyz]/g, '_')
+    // wordsLeft = 10
+    // remainingGuesses.textContent = wordsLeft
+    // incorrectLetters.textContent = ""
+    // correctGuess = []
+    // wrongGuess = []
+  reset()
+  } 
 
 }
 
-//Track game state
-function gameState() {
-if (wordToGuess !== str) {
-      incorrectLetters--
-    }
-
+function reset() {
+  previousWord.textContent = str
+    str = words[Math.floor(Math.random() * words.length)]
+    wordToGuess.textContent = str.replace(/[abcdefghijklmnopqrstuvwxyz]/g, '_')
+    wordsLeft = 10
+    remainingGuesses.textContent = wordsLeft
+    incorrectLetters.textContent = ""
+    correctGuess = []
+    wrongGuess = []
 }
 
-//how to reload game with variables reset? 
 
 
 
 
-  
 
- 
+
+
